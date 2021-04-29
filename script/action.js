80f23b28afb37cb6c5dbb0e61c590102ea57c94a
Melo-Melo-lab <74049040+Melo-Melo-lab@users.noreply.github.com>
2021-04-29 18:47:18 +0200

    melo update csss
    update des placements des boutons eet de la typo

--- script/action.js
+++ script/action.js
@@ -1,5 +1,297 @@
 var coin = -1; // Compteur pair & impair
 
+//DEFINITION DES BALISE HTML KNGHTS & Pages
+// const KR = "<img class="base" draggable="true" src="image/knight-red.svg" alt="" data-humain="knight" data-color="red">";
+// const PR = "<img class="base" draggable="true" src="image/page-red.svg" alt="" data-humain="page" data-color="red">";
+// const KB = "<img class="base" draggable="true" src="image/knight-blue.svg" alt="" data-humain="knight" data-color="blue">";
+// const PB = "<img class="base" draggable="true" src="image/page-blue.svg" alt="" data-humain="page" data-color="blue">";
+// const PY = "<img class="base" draggable="true" src="image/knight-yellow.svg" alt="" data-humain="knight" data-color="yellow">";
+// const PY = "<img class="base" draggable="true" src="image/page-yellow.svg" alt="" data-humain="page" data-color="yellow">";
+
+
+var attr = ["class", "draggable", "src", "data-humain", "data-color"];
+
+var valKR = ["base", "true", "image/knight-red.svg", "knight", "red"];
+var valPR = ["base", "true", "image/page-red.svg", "page", "red"];
+var valKB = ["base", "true", "image/knight-blue.svg", "knight", "blue"];
+var valPB = ["base", "true", "image/page-blue.svg", "page", "blue"];
+var valKY = ["base", "true", "image/knight-yellow.svg", "knight", "yellow"];
+var valPY = ["base", "true", "image/page-yellow.svg", "page", "yellow"];
+
+
+const leftTable11 = document.getElementById("111");
+const leftTable21 = document.getElementById("121");
+const leftTable12 = document.getElementById("112");
+const leftTable22 = document.getElementById("122");
+const leftTable13 = document.getElementById("113");
+const leftTable23 = document.getElementById("123");
+
+const rightTable11 = document.getElementById("211");
+const rightTable21 = document.getElementById("221");
+const rightTable22 = document.getElementById("222");
+const rightTable12 = document.getElementById("212");
+const rightTable23 = document.getElementById("223");
+const rightTable13 = document.getElementById("213");
+
+
+
+const listIdTable = ["111", "121", "112", "122", "113", "123", "firstCaseBoat", "secondCaseBoat", "211", "221", "222", "212", "223", "213"];
+const listIdTable2 = ["211", "221", "222", "212", "223", "213"];
+
+
+//INITIALISATION !!! 1er chiffre = Droite ou gauche; 2e chiffre = Column; 3e chiffre = Rangée
+var intialPositionKP = function() {
+
+  var caseTable = document.getElementsByTagName("td");
+
+  for (var i = 0; i < caseTable.length; i++) {
+    caseTable[i].innerHTML = "";
+  }
+
+
+  var KR = document.createElement("img");
+  for (let v = 0; v < attr.length; v++) {
+    KR.setAttribute(attr[v], valKR[v]);
+  }
+  leftTable11.append(KR);
+
+  var PR = document.createElement("img");
+  for (let v = 0; v < attr.length; v++) {
+    PR.setAttribute(attr[v], valPR[v]);
+  }
+  leftTable21.append(PR);
+
+  var KB = document.createElement("img");
+  for (let v = 0; v < attr.length; v++) {
+    KB.setAttribute(attr[v], valKB[v]);
+  }
+  leftTable12.append(KB);
+
+  var PB = document.createElement("img");
+  for (let v = 0; v < attr.length; v++) {
+    PB.setAttribute(attr[v], valPB[v]);
+  }
+  leftTable22.append(PB);
+
+  var KY = document.createElement("img");
+  for (let v = 0; v < attr.length; v++) {
+    KY.setAttribute(attr[v], valKY[v]);
+  }
+  leftTable13.append(KY);
+
+  var PY = document.createElement("img");
+  for (let v = 0; v < attr.length; v++) {
+    PY.setAttribute(attr[v], valPY[v]);
+  }
+  leftTable23.append(PY);
+}
+
+//ACTIVATION DE L'INITIALISATION
+intialPositionKP();
+document.getElementById("audio").play();
+
+
+//GAMEOVER
+
+var debbugSelector = document.getElementById("window-debbug");
+var gameOver = function(){
+  document.getElementById("hurt").play();
+  document.getElementById("cut").play();
+  document.getElementById("rame").pause();
+  document.getElementById("gameOver").style.display = "flex";
+  overlay.classList.add("active");
+}
+
+//CHECKER TABLE
+
+var checkerTable = function(){
+  if (coin%2 == 0) {
+    var actualTable = leftTable;
+  } else {
+    var actualTable = rightTable;
+  }
+
+  for (let i = 0; i < actualTable.rows.length; i++){
+    if (actualTable.rows[i].cells[1].firstChild === null) { //Peut-être double...
+      //pass
+    }
+    else if (actualTable.rows[i].cells[0].firstChild !== null && actualTable.rows[i].cells[1].firstChild !== null) {
+      //pass
+    }
+    else {
+      for (let p = 0; p < actualTable.rows.length; p++) {
+        if (actualTable.rows[p].cells[0].firstChild === null){
+          //pass
+        }
+        else {
+          gameOver();
+          console.log("GameOVer");
+        }
+      }
+    }
+  }
+}
+
+
+
+
+//CHECKER BOAT
+
+const firstCaseBoat = document.getElementById("firstCaseBoat");
+const secondCaseBoat = document.getElementById("secondCaseBoat");
+
+
+var checkerBoat = function(){
+  if (firstCaseBoat.childElementCount === 1) {
+
+    var firstElementBoat = firstCaseBoat.firstChild;
+    var secondElementBoat = secondCaseBoat.firstChild;
+
+    if (firstElementBoat === null || secondElementBoat === null) {
+      //pass
+    }
+    else if (firstElementBoat.dataset.humain === secondElementBoat.dataset.humain) {
+      //pass
+    } else {
+        if (firstElementBoat.dataset.color === secondElementBoat.dataset.color) {
+          //pass
+        } else {
+          gameOver();
+        }
+      }
+  } else {
+    //pass
+  }
+}
+
+
+//MOVE POSITION
+
+var moveElement = function(){
+
+  if (coin%2 == 0){
+    var tableNumber = 1; // Voir avec actualTable pour optimiser
+  } else {
+    var tableNumber = 2;
+  }
+
+//////////////////////////////// A OPTIMISER ////////////////////////////////
+  if (firstCaseBoat.firstChild !== null && secondCaseBoat.firstChild === null) {
+    if (firstCaseBoat.firstChild.dataset.humain == "knight"){
+      var tableColumn = 1;
+    } else {
+      var tableColumn = 2;
+    }
+    if (firstCaseBoat.firstChild.dataset.color == "red"){
+      var tableRow = 1;
+    }
+    else if (firstCaseBoat.firstChild.dataset.color == "blue") {
+      var tableRow = 2;
+    }
+    else {
+      var tableRow = 3;
+    }
+    var idCellPosition = tableNumber.toString() + tableColumn.toString()+ tableRow.toString();
+    var goToThere = document.getElementById(idCellPosition);
+    goToThere.append(firstCaseBoat.firstChild);
+  }
+
+  else if (firstCaseBoat.firstChild === null && secondCaseBoat.firstChild !== null) {
+    if (secondCaseBoat.firstChild.dataset.humain == "knight"){
+      var tableColumn = 1;
+    } else {
+      var tableColumn = 2;
+    }
+
+    if (secondCaseBoat.firstChild.dataset.color == "red"){
+      var tableRow = 1;
+    }
+    else if (secondCaseBoat.firstChild.dataset.color == "blue") {
+      var tableRow = 2;
+    }
+    else {
+      var tableRow = 3;
+    }
+    var idCellPosition = tableNumber.toString() + tableColumn.toString()+ tableRow.toString();
+    var goToThere = document.getElementById(idCellPosition);
+    goToThere.append(secondCaseBoat.firstChild);
+  }
+
+  else if (firstCaseBoat.firstChild !== null && secondCaseBoat.firstChild !== null) {
+    if (firstCaseBoat.firstChild.dataset.humain == "knight"){
+      var tableColumn = 1;
+    } else {
+      var tableColumn = 2;
+    }
+    if (firstCaseBoat.firstChild.dataset.color == "red"){
+      var tableRow = 1;
+    }
+    else if (firstCaseBoat.firstChild.dataset.color == "blue") {
+      var tableRow = 2;
+    }
+    else {
+      var tableRow = 3;
+    }
+    var idCellPosition = tableNumber.toString() + tableColumn.toString()+ tableRow.toString();
+    var goToThere = document.getElementById(idCellPosition);
+    goToThere.append(firstCaseBoat.firstChild);
+
+    if (secondCaseBoat.firstChild.dataset.humain == "knight"){
+      var tableColumn = 1;
+    } else {
+      var tableColumn = 2;
+    }
+
+    if (secondCaseBoat.firstChild.dataset.color == "red"){
+      var tableRow = 1;
+    }
+    else if (secondCaseBoat.firstChild.dataset.color == "blue") {
+      var tableRow = 2;
+    }
+    else {
+      var tableRow = 3;
+    }
+    var idCellPosition = tableNumber.toString() + tableColumn.toString()+ tableRow.toString();
+    var goToThere = document.getElementById(idCellPosition);
+    goToThere.append(secondCaseBoat.firstChild);
+  }
+
+  else { // sinon les deux cases sont vide IMPOSSIBLE
+    console.log("Erreur movePosition : condition impossible !!!")
+  }
+}
+
+var moveBoat = function(){
+
+  document.getElementById("rame").play();
+
+  if (coin%2 === 0) {
+    boat.classList.add('horizTranslateToLeft');
+    boat.classList.remove('horizTranslateToRight');
+  } else {
+    boat.classList.add('horizTranslateToRight');
+    boat.classList.remove('horizTranslateToLeft');
+  }
+}
+
+var moveCanvasBlock = function(){
+  if (coin%2 === 0) {
+    idCanvas.style.right = "O";
+    idCanvas.style.left = "";
+  }
+  else {
+    idCanvas.style.right = "";
+    idCanvas.style.left = "0";
+  }
+}
+
+
+var movePosition = function(){
+  moveCanvasBlock();
+  moveBoat();
+  moveElement();
+}
+
 // Fenêtre POPup https://www.youtube.com/watch?v=MBaw_6cPmAw
 
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
@@ -31,12 +323,14 @@ function openModal(modal) {
   if (modal == null) return
   modal.classList.add('active')
   overlay.classList.add('active')
+  document.getElementById("page").play();
 }
 
 function closeModal(modal) {
   if (modal == null) return
   modal.classList.remove('active')
   overlay.classList.remove('active')
+  document.getElementById("page").play();
 }
 
 
@@ -54,6 +348,7 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
+  document.getElementById("page").play();
   var i;
   var slides = document.getElementsByClassName("mySlides");
   if (n > slides.length) {slideIndex = 1}
@@ -63,6 +358,7 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
 }
+
 
 // Drag & Drop https://www.youtube.com/watch?v=-ah4LbthfYc
 //https://www.youtube.com/watch?v=tZ45HZAkbLc
@@ -114,14 +410,30 @@ for (let i = 0; i < bases.length; i++) {
 }
 
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////  EN TRAVAUX  ///////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//TRAVERSER
 
-/*
-// Bonton "Traverser"
+var traverserButton = document.getElementById("traverser");
 
-var buttonTraverser = document.getElementById("traverser");
+
+
+var TRAVERSER = function(){
+  if (firstCaseBoat.firstChild === null && secondCaseBoat.firstChild === null ){
+    console.log("Une des cases sont vides !!! Donc tu ne peux pas partir")
+  } else {
+  movePosition();
+  checkerTable();
+  checkerBoat();
+  console.log(coin);
+  coin+=1;
+  console.log(coin);
+  checkerTable();
+  winCondition();
+  }
+}
+
+
+traverserButton.addEventListener("click", TRAVERSER);
+
 
 var leftTable = document.getElementById("leftTable");
 var rightTable = document.getElementById("rightTable");
@@ -129,81 +441,31 @@ var rightTable = document.getElementById("rightTable");
 var idBoat = document.querySelector("#boat");
 var idCanvas = document.querySelector("#canvasBlock");
 
-var firstCaseBoat = document.getElementById("firstCaseBoat");
-var secondCaseBoat = document.getElementById("secondCaseBoat");
 
-var firstEmptyCellLeftTable = $('#leftTable td:empty:eq(0)'); // trouver la 1er case vide
-var firstEmptyCellRightTable = $('#rightTable td:empty:eq(0)'); // trouver la 1er case vide
+// Win condition listIdTable2
 
+var winCondition = function() {
+  var compteurElement = 0;
+  for(var w = 0; w < listIdTable2.length; w++){
+    var actualCell = listIdTable2[w];
 
-//checker tableau bateau
-var checkerBoat = function(){
-  if(firstCaseBoat.dataset.data-humain === secondCaseBoat.dataset.data-humain){
-    break;
-  }
-  else {
-    if(firstCaseBoat.dataset.data-color === secondCaseBoat.dataset.data-color){
-      break;
-    }
-    else {
-      gameOver();
+    if(document.getElementById(actualCell).firstChild !== null){
+      compteurElement += 1;
+      console.log("Compter win à  :", compteurElement);
+    } else {
+      //pass
     }
   }
-}
+  if(compteurElement == 6){
+    document.getElementById("flute").play();
+    document.getElementById("rame").pause();
+    document.getElementById("win").style.display = "flex";
+    overlay.classList.add("active");
 
-
-var moveHumain = function(){
-  var
-
+  } else {
+    //pass
   }
 }
-
-
-
-var cross = function(){ // Traverser + CHECKER
-  coin += 1;
-  if(coin%2 == 0){
-    //Checker tableau gauche
-
-
-    //checker tableau bateau
-    function checkerBoat();
-
-    //Transfert
-
-
-    //chercker tableau de gauche
-
-    // déplacer le canvasBlock
-    idBoat.style.left = "30vw";
-
-
-  }
-  else {
-
-  }
-
-    idBoat.style.left = "50vw";
-};
-
-buttonTraverser.addEventListener("click", cross);
-
-
-var gameOver = function() {
-  idGameOver.style.display = "block";
-}
-
-var idGameOver = document.querySelector("#gameOver");
-
-*/
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-// Compteur de traversé
-
-
-
-// Animation fleuve + arbres
 
 
 // Chrono
@@ -218,38 +480,66 @@ if (ti >= 0){
 
 var timer = window.setInterval(countUp, 1000);
 
-var stopCountUp = function() {
-  seconds.textContent = 0; //Remet le compteur à 0
-};
 
 
-//RESET
+//////////RESET//////////
 
 var resetButton = document.getElementById("reset");
-resetButton.addEventListener("click", stopCountUp);
+var resetButton2 = document.getElementById("reset2");
+var resetButton3 = document.getElementById("reset3");
+
+
+
+//Remet le compteur à 0
+var stopCountUp = function() {
+  seconds.textContent = 0;
+};
 
 //Traverser du bateau//
+
 
 var boatCurrentPosition = 0; // 0 : left, 1 : right
 var boat = document.getElementById("boat");
 
-var traverserFleuve = function(){
 
-    if (boatCurrentPosition == 0){
-	boat.classList.add('horizTranslateToRight');
-	boat.classList.remove('horizTranslateToLeft');
-	boatCurrentPosition = 1;
-    }
-    else{
-	boat.classList.add('horizTranslateToLeft');
-	boat.classList.remove('horizTranslateToRight');
-	boatCurrentPosition = 0;
-    }
+var traverserFleuve = function(){​​​​​​​
 
 
+    if (boatCurrentPosition == 0){​​​​​​​
+    boat.classList.add('horizTranslateToRight');
+    boat.classList.remove('horizTranslateToLeft');
+    boatCurrentPosition = 1;
+    }​​​​​​​
+    else{​​​​​​​
+    boat.classList.add('horizTranslateToLeft');
+    boat.classList.remove('horizTranslateToRight');
+    boatCurrentPosition = 0;
+    }​​​​​​​
 
-};
+}​​​​​​​;
+
 
 
 var traverserButton = document.getElementById("traverser");
 traverserButton.addEventListener("click", traverserFleuve);
+
+
+//Remet le bateau à ca position initial
+var intialBoat = function() {
+  idBoat.style.left = "25vw";
+}
+
+// Remet le BlockCanvas à ca position initial
+var intialCanvasBlock = function() {
+  idCanvas.style.right = "O";
+  idCanvas.style.left = "";
+}
+
+// FONCTION CALL
+var RESET = function() {
+  document.location.reload();
+}
+
+resetButton.addEventListener("click", RESET);
+resetButton2.addEventListener("click", RESET);
+resetButton3.addEventListener("click", RESET);
